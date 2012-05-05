b4a22599d0441670af3eafd814faf89c216dbf14
smiley.mikey@gmail.com <smileymike@ubuntu.(none)>
2012-05-05 23:08:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BffmApp::Application.config.secret_token = 'f7a19811939e5fedaf51653c1a3fa8d99cbef1168f65d7605c5ce0099b9e91116cc4b977502affb5a54f5ecfe9835119dcb67d48174efeb5254a5fc1a5f17f22'
